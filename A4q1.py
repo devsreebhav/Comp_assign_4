a265532947c3df3d96050ad0e97d31697afdd956
devsreebhav <164992725+devsreebhav@users.noreply.github.com>
2024-05-27 23:29:23 +0530

    Update A4q1.py

--- A4q1.py
+++ A4q1.py
@@ -1,2 +1,26 @@
+import numpy as np
+np.set_printoptions(threshold=np.inf)
+import scipy as sp
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.use(preferred_backend)
+a=1104678569
+c=10234
+m=2037586678
+x=13
+x=15
+n=10000
+random_nos=[]
+for i in range(n):
+  x=(a*x+c)%m
+  random_nos.append(x/m)
+plt.hist(random_nos,density ="True",label="Uniform random numbers")
+x=np.arange(0,1.1,0.1)
+y=np.ones(x.size)
+plt.plot(x,y,label="Uniform PDF")
+plt.xlabel("x",fontsize=17)
+plt.ylabel("PDF",fontsize=17)
+plt.legend(fontsize=17)
+plt.show()
 
 
